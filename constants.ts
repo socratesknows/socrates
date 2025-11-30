b6695a093878b77febf895247dedaaca365e3040
socrates <arnavsinharn123@gmail.com>
2025-11-30 13:37:48 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -12,20 +12,12 @@ export const BLOG_POSTS: BlogPost[] = [
   {
     id: '1',
     slug: 'the-examined-life',
-    title: 'the concept of overthinking',
+    title: 'the concept of systems',
     date: '2025-11-29',
-    description: 'the hard to control recursive evaluation of false justification',
-    content: `the whole concept of overthinking is based on the foundation of false justification. you're in this constant iterated loop of trying to escape the task at hand by making
-    unreasonable statements in your mind.
-    
+    description: 'the machines that work for you',
+    content: `
 
-the simplest solution is by simply starting, i think the "fuck it" method works best here. whenever you feel yourself overthinking and finding reasons to skip the task at hand, simply say
-"fuck it" and start doing it. gradually, the task will become easier, and you will find yourself making progress
 
-a common trigger for overthinking is overcomplicating. it's a pretty simple fix that takes some time to perfect.
-
-"simplify"
-
-through the process of breaking down complex tasks into simpler, more achievable bits, you're creating a system for success.`
+`
   },
 ];
