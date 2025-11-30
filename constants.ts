f6bc15bb07901f101ff80a8c3eb400af3daf505c
socrates <arnavsinharn123@gmail.com>
2025-11-30 14:58:37 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -15,7 +15,23 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'the concept of systems ',
     date: '2025-11-29',
     description: 'building iterated machines that guide you in achieving things',
-    content: ` to acheive your goals, it's important you build systems.`
+    content: ` to acheive your goals, it's important you build systems.
+    
+having goals gives you direction, while systems give you a path to get to those goals. systems work like machines. you trigger them, they crank out a whole bunch of processes, all
+with the purpose of doing one thing, producing results in the form of a product.
+
+how to build systems?
+
+begin by defining your goal. what is it that you want? why do you want it?
+
+after that, depending on the type of goal, you must build out processes. the key to building processes is by thinking about what it would take to achieve the goal. what resources do i require?
+what steps must i take?
+
+then split all the processes into smaller, less complex tasks. 
+
+don't overcomplicate anything, create simple systems to achieve great goals. use your ambition as fuel to begin working. 
+
+`
 
   },
 ];
