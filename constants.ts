1cb912ffd8c6c11988c82073676b42ceb04c9e4d
socrates <arnavsinharn123@gmail.com>
2025-12-01 13:36:20 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -15,17 +15,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'iteration',
     date: '2025-11-29',
     description: 'the correct way of learning and how to practice it',
-    content: ` learning through iterated moves is the only optimal form of learning. if you are learning for the sole purpose of memorisation, you might as well
-    not learn.
-
-
-iteration means making constannt improvements in your system of learning through understanding what works and what doesn't. if you were to face an obstacle, someone who
-iterates would learn from the obstacle, make changes in their system to optimise it so that the next time they face a similar obstacle, they know their way around it
-
-
-iteration also opens up doors for creativity. 
-    
-
+    content: ` 
 
 `
 
